99ea3e46be1125e4d6783da443498a440774df60
gagandeepramghariya <gagandeepvirdi2005@gmail.com>
2025-12-21 11:18:37 +0530

    add code

---
+++ second.py
@@ -0,0 +1,22 @@
+ 
+import random
+while True:
+    print("1. guess a number")
+    print("2. show score")
+    print("3. exit")
+    choice=int(input("Enter your choice: "))
+    if choice==1:
+        guess = int(input("Enter a number between 1 and 10: "))
+        if guess <1 or guess >10:
+            print("Invalid input. Please try again.")
+            continue 
+    elif choice==2:
+        pass 
+    elif choice==3:
+        break
+
+
+     
+     
+         
+            
